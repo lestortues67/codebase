4a8e88b9e398f168928f79e7411385011c96c1a1
“christian <christian.doriath@free.Fr>
2024-02-14 11:40:30 +0100

    tous les fichiers

---
+++ packages/mysql.py
@@ -0,0 +1,27 @@
+"""
+Source : 
+Date : 30/01/2019
+Auteur : Christian Doriath
+Dossier : /Python34/MesDEv/Flask/FlaskBootstrap_BASE
+Fichier : classes.py
+Description : Identifiants pour la dbase ''
+
+Mot cles : 
+"""
+
+
+username_mysql="lestortues67"
+password_mysql="PAW/mxSard23"
+hostname_mysql="lestortues67.mysql.eu.pythonanywhere-services.com"
+databasename_mysql="lestortues67$codebase"
+
+
+# local variables : 
+# username="root"
+# password="root"
+# hostname="localhost"
+# databasename="codebase"
+# app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+mysqlconnector://root:root@localhost/codebase'
+
+
+
